7d76aac2eeb838489cf142c2f1a76d5fddd3c6f9
ADITHYAN MB <151754943+ADI-MB-05@users.noreply.github.com>
2025-10-27 15:25:08 +0530

    commit exp-18

--- Experiment-18/exp-rbac/middleware/authmiddleware.js
+++ Experiment-18/exp-rbac/middleware/authmiddleware.js
@@ -3,7 +3,7 @@ const SECRET_KEY = "mysecretkey";
 
 function verifyToken(req, res, next) {
   const token = req.headers["authorization"];
-  if (!token) return res.status(403).json({ error: "No token provided." });
+  if (!token) return res.status(403).json({ error: "Access denied. No token provided." });
 
   try {
     const decoded = jwt.verify(token.split(" ")[1], SECRET_KEY);
@@ -17,7 +17,7 @@ function verifyToken(req, res, next) {
 function authorizeRoles(...roles) {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ error: "Forbidden. You don’t have access." });
+      return res.status(403).json({ error: "Forbidden. You do not have permission." });
     }
     next();
   };
