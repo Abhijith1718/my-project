8949f8129b0727f416bf74c18fae2e95008183af
Abhijith N <abhijithnambrath1718@gmail.com>
2025-11-08 13:50:23 +0530

    Adding pending exp

--- rewrite_author.py
+++
@@ -1,8 +0,0 @@
-def callback(commit, metadata):
-    if commit.author_name == b"ADI-MB-05":
-        commit.author_name = b"Abhijith1718"
-        commit.author_email = b"abhijithnambrath1718@gmail.com"
-    if commit.committer_name == b"ADI-MB-05":
-        commit.committer_name = b"Abhijith1718"
-        commit.committer_email = b"abhijithnambrath1718@gmail.com"
-    return commit
